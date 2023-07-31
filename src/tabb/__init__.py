3665aea2bbd738997b2d50e39473460e4eea24a7
William T Olson <trevor@hiringsolved.com>
2023-07-30 18:03:19 -0700

    Expose api

---
+++ src/tabb/__init__.py
@@ -0,0 +1,62 @@
+from tabb.__about__ import __version__
+from tabb.command import Command
+from tabb.config import Config
+from tabb.context import Context
+from tabb.decorators import command, group
+from tabb.exceptions import (
+    Abort,
+    BadParameter,
+    Exit,
+    Help,
+    MissingParameter,
+    TabbError,
+    UnexpetedParameter,
+    UsageError,
+)
+from tabb.group import Group
+from tabb.missing import MISSING, is_missing
+from tabb.nargs import Nargs, NArgsLiteral
+from tabb.types import (
+    Argument,
+    Choices,
+    Depends,
+    Greedy,
+    Length,
+    Matches,
+    Option,
+    Range,
+    Secret,
+    Validate,
+)
+
+__all__ = [
+    "__version__",
+    "Abort",
+    "Argument",
+    "BadParameter",
+    "Choices",
+    "command",
+    "Command",
+    "Config",
+    "Context",
+    "Depends",
+    "Exit",
+    "Greedy",
+    "group",
+    "Group",
+    "Help",
+    "is_missing",
+    "Length",
+    "Matches",
+    "MISSING",
+    "MissingParameter",
+    "Nargs",
+    "NArgsLiteral",
+    "Option",
+    "Range",
+    "Secret",
+    "TabbError",
+    "UnexpetedParameter",
+    "UsageError",
+    "Validate",
+]
